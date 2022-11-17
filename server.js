63166cb71bfed23f55e184ef4c9d25fa73c44254
pmurphy926 <patrick.murphy926@gmail.com>
2022-11-17 11:43:16 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+//*******************DEPENDENCIES********************/
+const express = require('express');
+const app = express();
+
+let PORT = 3000
+if(process.env.PORT) {
+    PORT = process.env.PORT
+}
+
+app.get('/', (req, res) => {
+    res.send('hi')
+})
+
+app.listen(3000, () => {
+    console.log('listening...')
+})
